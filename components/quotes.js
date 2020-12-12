6723755830eecb388593e859f9192c45d235159d
bmumz <brandimummery@gmail.com>
2020-12-12 00:36:59 -0500

    Initial commit.

--- components/quotes.js
+++ components/quotes.js
@@ -10,24 +10,10 @@ export default function Quotes() {
 
   const quote = data.data[0].quote;
   const source = data.data[0].source;
-  console.log(quote, source);
 
   return (
     <div>
-      <h1>{quote}</h1> <h3>{source}</h3>
+      <h2>"{quote}"</h2> <h3>{source}</h3>
     </div>
   );
 }
-
-// export async function getStaticProps() {
-//   const request = await fetch(
-//     "https://inspirational-quotes-api.herokuapp.com/quotes"
-//   );
-//   const json = await request.json();
-
-//   return {
-//     props: {
-//       data: json.data,
-//     },
-//   };
-// }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Head from "next/head";
 import Layout from "../components/layout";
 import Quotes from "../components/quotes";
+import { siteTitle } from "../components/layout";
 
 export default function Home() {
   return (
     <div>
       <Layout>
         <Head>
-          <title>You Go, Girl!</title>
+          <title>{siteTitle}</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <main>
-          <h1 className="title">Your source for a pick me up!</h1>
+          <h1 className="title">{siteTitle}</h1>
+          <h4>Your daily dose of inspiration!</h4>
           <Quotes />
         </main>
       </Layout>
